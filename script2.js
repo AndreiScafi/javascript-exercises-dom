5c9dd8e35b60e37837e1fd6ee1e45691fc57bb0e
Andrei Scafi <andreiscafi@hotmail.com>
2023-05-09 07:59:19 -0300

    Objeto Window no Javascript. conceito e aplicação.

---
+++ script2.js
@@ -0,0 +1,19 @@
+// Window
+
+//Conceito:
+
+/* 
+Em JavaScript, o objeto Window representa a janela que contém um documento em um navegador da web. Ele é a raiz do modelo de objeto do navegador e, portanto, fornece acesso a muitos recursos úteis do navegador, como a barra de endereços, histórico de navegação, controle de janelas e muito mais.
+
+O objeto Window também é responsável por gerenciar o ciclo de vida da janela do navegador, incluindo a criação, carregamento, fechamento e redimensionamento da janela.
+
+Além disso, o objeto Window é o escopo global para código JavaScript em uma página da web. Isso significa que qualquer variável ou função definida globalmente estará acessível a partir do objeto Window.
+
+Em resumo, o objeto Window é um dos principais objetos em JavaScript para interagir com o ambiente do navegador e é fundamental para muitos recursos da web moderna.
+*/
+
+//Como window é o objeto padrão do javascript, por isso não precisa ser chamado sem a necessidade de escrever window na sintaxe:
+
+window.console.log('Objeto Window com objeto console e método log.');
+// Pode ser chamado sem escrever window na sintaxe.
+console.log('Objeto Window com objeto console e método log. Mas sem escrever window na sintaxe.')
