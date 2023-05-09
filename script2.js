9bbbf469e283d9103c8d5018e04859a2950fa773
Andrei Scafi <andreiscafi@hotmail.com>
2023-05-09 08:29:13 -0300

    Objeto document, conceito e aplicação.

--- script2.js
+++ script2.js
@@ -16,4 +16,32 @@ Em resumo, o objeto Window é um dos principais objetos em JavaScript para inter
 
 window.console.log('Objeto Window com objeto console e método log.');
 // Pode ser chamado sem escrever window na sintaxe.
-console.log('Objeto Window com objeto console e método log. Mas sem escrever window na sintaxe.')
+console.log('Objeto Window com objeto console e método log. Mas sem escrever window na sintaxe.');
+
+// Document
+
+// Conceito: 
+
+/* 
+Em JavaScript, o objeto "document" representa o documento HTML que está sendo exibido em um navegador da web. É a interface principal para manipulação e navegação de elementos HTML na página.
+
+O objeto "document" é criado automaticamente pelo navegador ao carregar uma página HTML e oferece uma ampla variedade de métodos e propriedades que permitem a manipulação dinâmica da página.
+
+Algumas das funções mais comuns do objeto "document" incluem:
+*/
+
+/* 
+getElementById() // retorna o elemento HTML que possui o ID especificado
+getElementsByTagName()//retorna uma lista de elementos HTML com o nome de tag especificado
+querySelector() // retorna o primeiro elemento HTML que corresponde ao seletor CSS especificado
+createElement() // cria um novo elemento HTML especificado pelo nome da tag
+createTextNode()// cria um novo nó de texto com o texto especificado 
+*/
+
+/*
+Além dessas, existem muitas outras funções e propriedades disponíveis no objeto "document", que permitem a manipulação dinâmica do conteúdo da página, como alterar o conteúdo de um elemento HTML, adicionar ou remover elementos HTML, manipular estilos CSS e muito mais.
+
+Em resumo, o objeto "document" é fundamental para a interação com o conteúdo HTML em uma página da web e é um dos principais objetos do DOM (Modelo de Objeto do Documento) em JavaScript.
+*/
+
+console.log(document) // contém todo o HTML
